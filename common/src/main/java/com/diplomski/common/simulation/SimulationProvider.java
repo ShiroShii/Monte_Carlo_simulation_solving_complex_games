31b1c6b8a00fcd586ade2118b933bafc6e6570ed
Nemanja Cetkovic <cetko.ns@gmail.com>
2021-05-10 19:04:35 +0100

    SimulationProvider

---
+++ common/src/main/java/com/diplomski/common/simulation/SimulationProvider.java
@@ -0,0 +1,29 @@
+package com.diplomski.common.simulation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.diplomski.common.battle.Battle;
+import com.diplomski.common.battle.IBattleProvider;
+import com.diplomski.common.character.CharacterState;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class SimulationProvider implements ISimulationProvider {
+	
+	private final IBattleProvider battleProvider;
+
+	@Override
+	public Simulation getSimulation(List<CharacterState> initialCharacterState, int simulationCount) {
+		List<Battle> battles = new ArrayList<>();
+		
+		//parallelize?
+		for(int i=0; i<simulationCount; i++) {
+			battles.add(battleProvider.getBattle(initialCharacterState));
+		}
+		
+		return Simulation.builder().battles(battles).initialCharacterStates(initialCharacterState).build();
+	}
+
+}
---
+++ common/src/test/java/com/diplomski/common/simulation/SimulationProviderTest.java
@@ -0,0 +1,54 @@
+package com.diplomski.common.simulation;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.diplomski.common.battle.Battle;
+import com.diplomski.common.battle.IBattleProvider;
+import com.diplomski.common.character.CharacterState;
+
+public class SimulationProviderTest {
+	private IBattleProvider battleProviderMock = mock(IBattleProvider.class);
+	private int expectedBattleCount = 3;
+	private Battle battle1;
+	private Battle battle2;
+	private Battle battle3;
+
+	private List<Battle> battles;
+
+	private List<CharacterState> characterStates;
+	private Simulation expectedSimulation;
+	private SimulationProvider unitUnderTest;
+
+	@Before
+	public void setup() {
+		battle1 = Battle.builder().build();
+		battle2 = Battle.builder().build();
+		battle3 = Battle.builder().build();
+
+		battles = Arrays.asList(battle1, battle2, battle3);
+
+		characterStates = new ArrayList<>();
+
+		expectedSimulation = Simulation.builder().battles(battles).initialCharacterStates(characterStates).build();
+
+		when(battleProviderMock.getBattle(eq(characterStates))).thenReturn(battle1).thenReturn(battle2)
+				.thenReturn(battle3);
+		unitUnderTest = new SimulationProvider(battleProviderMock);
+	}
+
+	@Test
+	public void getSimulation() {
+		Simulation result = unitUnderTest.getSimulation(characterStates, expectedBattleCount);
+		assertEquals(expectedSimulation, result);
+		verify(battleProviderMock, times(3)).getBattle(eq(characterStates));
+	}
+
+}
